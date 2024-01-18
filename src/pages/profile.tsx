e6b300174c8b2baa3219c617ce8037543b4f65f8
abdrakhmanovzh <abdrakhmanovzh@gmail.com>
2024-01-19 00:30:12 +0600

    feat: add logout

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/shared/components/ui/button'
 import { useAuthStore } from '@/modules/auth'
 
 export const ProfilePage = () => {
   const user = useAuthStore((state) => state.user)
+  const signout = useAuthStore((state) => state.signout)
 
   return (
     <div className="flex flex-1 gap-40 px-[5%]">
@@ -10,6 +12,10 @@ export const ProfilePage = () => {
         <hr />
         <p className="text-lg lowercase text-neutral-600">name: {user?.name}</p>
         <p className="text-lg lowercase text-neutral-600">email: {user?.email}</p>
+
+        <Button variant={'outline'} onClick={signout}>
+          sign out
+        </Button>
       </div>
 
       <div className="flex flex-1 flex-col gap-4">
