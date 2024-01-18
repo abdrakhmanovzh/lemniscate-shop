d661bea96d44a14aaf85b69f4cd472badd5300ce
abdrakhmanovzh <abdrakhmanovzh@gmail.com>
2024-01-19 00:31:56 +0600

    fix: signout navigate issue

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,19 +1,27 @@
 import { Button } from '@/shared/components/ui/button'
+import { useNavigate } from 'react-router-dom'
 import { useAuthStore } from '@/modules/auth'
 
 export const ProfilePage = () => {
   const user = useAuthStore((state) => state.user)
   const signout = useAuthStore((state) => state.signout)
 
+  const navigate = useNavigate()
+
+  const handleSignout = async () => {
+    await signout()
+    navigate('/')
+  }
+
   return (
     <div className="flex flex-1 gap-40 px-[5%]">
       <div className="flex w-fit flex-col gap-4">
-        <h1 className="text-3xl font-semibold">my pofile</h1>
+        <h1 className="text-3xl font-semibold">my profile</h1>
         <hr />
         <p className="text-lg lowercase text-neutral-600">name: {user?.name}</p>
         <p className="text-lg lowercase text-neutral-600">email: {user?.email}</p>
 
-        <Button variant={'outline'} onClick={signout}>
+        <Button onClick={handleSignout} variant={'outline'}>
           sign out
         </Button>
       </div>
